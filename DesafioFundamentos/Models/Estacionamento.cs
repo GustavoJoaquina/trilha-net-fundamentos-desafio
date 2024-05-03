10dad643767bd1f8be653d47d4f78af9924dc22b
Gustavo Ryan Joaquina <gugagg002@gmail.com>
2024-05-02 21:19:03 -0300

    adicionar e listar veiculos

--- DesafioFundamentos/Models/Estacionamento.cs
+++ DesafioFundamentos/Models/Estacionamento.cs
@@ -17,6 +17,8 @@ namespace DesafioFundamentos.Models
             // TODO: Pedir para o usuário digitar uma placa (ReadLine) e adicionar na lista "veiculos"
             // *IMPLEMENTE AQUI*
             Console.WriteLine("Digite a placa do veículo para estacionar:");
+            string item = Console.ReadLine();
+            veiculos.Add(item);
         }
 
         public void RemoverVeiculo()
@@ -57,6 +59,9 @@ namespace DesafioFundamentos.Models
                 Console.WriteLine("Os veículos estacionados são:");
                 // TODO: Realizar um laço de repetição, exibindo os veículos estacionados
                 // *IMPLEMENTE AQUI*
+                foreach (string veiculo in veiculos){
+                    Console.WriteLine($"{veiculo}");
+                }
             }
             else
             {
